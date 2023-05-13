a77c395947d25bbd495099020d2758a684b3808f
andresoaresmota <andresoaresmota@gmail.com>
2023-05-13 15:59:01 -0300

    atualizacoes render src/pages/produtos/index.jsx (para usar render) e src routes/indes.js (para nao carregar 2 vezes noconsole.log)

--- src/pages/Produtos/index.jsx
+++ src/pages/Produtos/index.jsx
@@ -2,7 +2,14 @@ import estilo from './styles.module.scss'
 import Filtrar from '../../components/MenuFiltrar'
 import Produtos from '../../components/ProdutoCard'
 import Ordenar from '../../components/MenuOrdenar'
+import { useEffect } from 'react'
 export default function ProdutoHome() {
+
+    useEffect(()=>{console.log(1)
+    fetch('https://loja-backend.onrender.com/produtos').then(res => res.json()).then(data => console.log(data))
+
+    }, [])
+
     return (
         <div className={estilo.teste}>
             <div className={estilo.container}>
