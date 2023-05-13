a243767b4d90117aa6ba9c1f69e4f476883b3456
angelolustosa <angelolustosa@gmail.com>
2023-05-13 15:16:47 -0300

    chuchu https

--- src/pages/Produtos/index.jsx
+++ src/pages/Produtos/index.jsx
@@ -2,35 +2,47 @@ import estilo from './styles.module.scss'
 import Filtrar from '../../components/MenuFiltrar'
 import Produtos from '../../components/ProdutoCard'
 import Ordenar from '../../components/MenuOrdenar'
+import { useEffect } from 'react'
 export default function ProdutoHome() {
-    return (
-        <div className={estilo.teste}>
-            <div className={estilo.container}>
-                <div className={estilo.Ordenar}>
-                    <Ordenar />
-                </div>
-                <div className={estilo.containerProdutos}>
-                    <div className={estilo.menuFiltrar}>
-                        <Filtrar />
-                    </div>
-                    <div className={estilo.produtos}>
-                        <Produtos />
-                        <Produtos />
-                        <Produtos />
-                        <Produtos />
-                        <Produtos />
-                        <Produtos />
-                        <Produtos />
-                        <Produtos />
-                        <Produtos />
-                        <Produtos />
-                        <Produtos />
-                        <Produtos />
-                        <Produtos />
-                        <Produtos />
-                        <Produtos />
-                    </div>
 
+    useEffect(() => {
+        console.log(1)
+        fetch('https://backend-dc-server.onrender.com/produtos').then(res => res.json()).then(data => console.log(data))
+    }, [])
+
+    return (
+        <div style={{ border: '1px red solid' }}>
+            <div className={estilo.teste}>
+                <div className={estilo.container}>
+                    <div className={estilo.Ordenar}>
+                        <Ordenar />
+                    </div>
+                    <div className={estilo.containerProdutos}>
+                        <div className={estilo.menuFiltrar}>
+                            <Filtrar />
+                        </div>
+                        <div className={estilo.produtos}>
+                            1
+                            <Produtos />
+                            2
+                            <Produtos />
+                            3
+                            <Produtos />
+                            <Produtos />
+                            <Produtos />
+                            <Produtos />
+                            <Produtos />
+                            <Produtos />
+                            <Produtos />
+                            <Produtos />
+                            <Produtos />
+                            <Produtos />
+                            <Produtos />
+                            <Produtos />
+                            <Produtos />
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </div>
